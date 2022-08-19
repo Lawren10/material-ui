92d3a8ba22623afe3b0843dcd3545e4872f631d7
d-enigma <lawrenceanthony70@gmail.com>
2022-08-19 16:46:12 +0100

    buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import {Typography} from "@mui/material";
 import {CustomDiv} from "./components/styledMaterial";
+import {Stack, Button} from "@mui/material";
 
 function App() {
   return (
     <div>
       <Typography variant="h1">Material Design</Typography>
       <CustomDiv></CustomDiv>
+      <Stack direction="row" spacing={2} marginY="2rem">
+        <Button variant="outlined">outlined</Button>
+        <Button variant="contained">default</Button>
+        <Button variant="text">text</Button>
+      </Stack>
     </div>
   );
 }
